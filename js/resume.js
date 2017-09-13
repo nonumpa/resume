f24b202ee9de0a9d690943f18590efea3d96a7b3
nonumpa <acerxp511@msn.com>
2017-09-13 15:05:45 +0800

    Initial Commit
    Initial Commit

---
+++ js/resume.js
@@ -0,0 +1,260 @@
+
+var portfolioData = 
+  {
+    web: [{
+      title: "自我介紹網頁",
+      description: "練習網頁前端設計，把我的履歷，做成一個網頁。",
+      coverImg: "https://c1.staticflickr.com/5/4211/34947361342_f6d032bb98_o.png",
+      projectLink: "https://codepen.io/nonumpa/pen/OmbwaZ?editors=1010",
+      youtubeLink: ""
+    },
+    {
+      title: "天氣盒子",
+      description: "使用純CSS和SVG分別刻出太陽和雲，再套用darksky提供的天氣API，顯示我目前住的地方，7天內的天氣概況。",
+      coverImg: "https://c1.staticflickr.com/5/4266/34268020614_a3b5b348b5_o.png",
+      youtubeLink: "",
+      projectLink: "https://codepen.io/nonumpa/full/zwoYdx/"
+    }],
+    
+    game: [{
+      title: "倖存者聯盟",
+      description: "使用Unity3D開發，開發團隊人數約20人，主要的玩法跟部落衝突類似，裡面加入末日殭屍主題做一點微創新，是在前公司參與的第一個專案，我加入團隊時，專案已經進入中後期開發，主要負責一些新UI系統開發(使用NGUI)、使用Unity的plugin重接金流系統。",
+      coverImg: "https://lh3.googleusercontent.com/LFGfC-EogH4N5iS19weIJHK3X7l8KZPG_S-OcdZRsZG_EoZ4ptNtK9RW8np4Un8zxw=h900-rw",
+      youtubeLink: "",
+      projectLink: "https://play.google.com/store/apps/details?id=com.igs.clashz&hl=zh_TW"
+    },
+    {
+      title: "生存之靈",
+      description: "大學課程Project，開發人數4人，使用Unity3D開發，生存之靈(3D版小精靈)是一款冒險收集的遊戲，依照玩家對於遊戲長短的需求來選取關卡，在地圖上隨時都會有危機以及廝殺，玩家將扮演逃亡的靈魂體必須利用時間快速地收集到所以散落四地的靈魂獲得重生並且過關，過程使用手機螢幕滑動來做不同靈巧的判斷及動作，在惡魔的地牢安全的生存下去，刺激和挑戰集於此，相信玩家會愛不釋手的去挑戰！",
+      coverImg: "https://c1.staticflickr.com/5/4201/34947329142_773f98358d_o.png",
+      youtubeLink: "https://www.youtube.com/embed/Keu_kJDP8e0",
+      projectLink: ""
+    },
+    {
+      title: "骰子迷城",
+      description: "使用Unity3D開發，開發人數3人，是大三時跟宇峻奧汀產學合作的專案，屬於DEMO版本，使用骰子在玩家和怪物之間走出一條路發動攻擊，如果走出來的點數符合旁邊技能表可使出額外技能攻擊。",
+      coverImg: "",
+      youtubeLink: "https://www.youtube.com/embed/hAtQ9iODwLA",
+      projectLink: ""
+    },
+    {
+      title: "黑白棋",
+      description: "大學課程Project，使用Haaf's Game Engine(HGE)製作。",
+      coverImg: "",
+      youtubeLink: "https://www.youtube.com/embed/WDO3TWmyp5M",
+      projectLink: ""
+    }],
+    video: [{
+      title: "大學龍捲風",
+      description: "大二上學期參與校外社團，幫忙製作的宣傳影片，負責剪輯(Premiere)和劇本。片頭是夥伴使用AE做的，搭配上配樂效果很酷，未來如果有空的話，想試著學看看。",
+      coverImg: "",
+      youtubeLink: "https://www.youtube.com/embed/nuF4i4_Z8Jo",
+      projectLink: ""
+    },
+    {
+      title: "芯芯疑似盜版歌曲1",
+      description: "Love Me Like You Do，大學畢業後，等當兵前，無聊看台順便幫忙剪輯，因為沒什麼美感，所以做的不是很漂亮，這部影片末端加入當時紅極一時的柯P敲桌子，笑果還不錯。",
+      coverImg: "",
+      youtubeLink: "https://youtube.googleapis.com/embed/?status=ok&hl=zh_TW&allow_embed=0&ps=docs&partnerid=30&autoplay=0&docid=0BwDP-d-CvtjxMGZncHlWaWJfeFU&abd=0&public=false&el=leaf&title=sinsin-love%20me%20like%20you%20do.mp4&BASE_URL=https%3A%2F%2Fdrive.google.com%2F&iurl=https%3A%2F%2Fdrive.google.com%2Fvt%3Fauthuser%3D0%26id%3D0BwDP-d-CvtjxMGZncHlWaWJfeFU&reportabuseurl=https%3A%2F%2Fdrive.google.com%2Fabuse%3Fauthuser%3D0%26id%3D0BwDP-d-CvtjxMGZncHlWaWJfeFU&token=1&plid=V0QUvQAaioas2A&timestamp=1494350766902&length_seconds=218&BASE_YT_URL=https%3A%2F%2Fdrive.google.com%2F&cc_load_policy=1&authuser=0&cc3_module=1&wmode=window&override_hl=1&enablecastapi=0&enablepostapi=1&postid=drive-viewer-video-player-object-0&origin=https%3A%2F%2Fdrive.google.com",
+      projectLink: ""
+    },
+    {
+      title: "芯芯疑似盜版歌曲2",
+      description: "Blank Space，大學畢業後，等當兵前，無聊看台順便幫忙剪輯，因為沒什麼美感，所以做的不是很漂亮。",
+      coverImg: "",
+      youtubeLink: "https://youtube.googleapis.com/embed/?status=ok&hl=zh_TW&allow_embed=0&ps=docs&partnerid=30&autoplay=0&docid=0BwDP-d-CvtjxRWFaNzBNOEd0SW8&abd=0&public=false&el=leaf&title=sinsin-blank%20space(beta).mp4&BASE_URL=https%3A%2F%2Fdrive.google.com%2F&iurl=https%3A%2F%2Fdrive.google.com%2Fvt%3Fauthuser%3D0%26id%3D0BwDP-d-CvtjxRWFaNzBNOEd0SW8&reportabuseurl=https%3A%2F%2Fdrive.google.com%2Fabuse%3Fauthuser%3D0%26id%3D0BwDP-d-CvtjxRWFaNzBNOEd0SW8&token=1&plid=V0QUvQB7_Teblw&timestamp=1494351183955&length_seconds=261&BASE_YT_URL=https%3A%2F%2Fdrive.google.com%2F&cc_load_policy=1&authuser=0&cc3_module=1&wmode=window&override_hl=1&enablecastapi=0&enablepostapi=1&postid=drive-viewer-video-player-object-0&origin=https%3A%2F%2Fdrive.google.com",
+      projectLink: ""
+    },
+    {
+      title: "芯芯疑似盜版歌曲3",
+      description: "Chandelier，大學畢業後，等當兵前，無聊看台順便幫忙剪輯，因為沒什麼美感，所以做的不是很漂亮。",
+      coverImg: "",
+      youtubeLink: "https://youtube.googleapis.com/embed/?status=ok&hl=zh_TW&allow_embed=0&ps=docs&partnerid=30&autoplay=0&docid=0BwDP-d-CvtjxQmMtMTRmWGlzck0&abd=0&public=false&el=leaf&title=%E8%8A%AF%E8%8A%AF-chandelier%20(live%20cover).mp4%20%E7%9A%84%E5%89%AF%E6%9C%AC&BASE_URL=https%3A%2F%2Fdrive.google.com%2F&iurl=https%3A%2F%2Fdrive.google.com%2Fvt%3Fauthuser%3D0%26id%3D0BwDP-d-CvtjxQmMtMTRmWGlzck0&reportabuseurl=https%3A%2F%2Fdrive.google.com%2Fabuse%3Fauthuser%3D0%26id%3D0BwDP-d-CvtjxQmMtMTRmWGlzck0&token=1&plid=V0QUvQCShUNMWQ&timestamp=1494351281404&length_seconds=221&BASE_YT_URL=https%3A%2F%2Fdrive.google.com%2F&cc_load_policy=1&authuser=0&cc3_module=1&wmode=window&override_hl=1&enablecastapi=0&enablepostapi=1&postid=drive-viewer-video-player-object-0&origin=https%3A%2F%2Fdrive.google.com",
+      projectLink: ""
+    }
+    ],
+    cg: [{
+      title: "Face Talk",
+      description: "研究所課程Project，輸入一張照片，手動加入控制點，做as rigid as possible  2D變形，把苦臉被笑臉。起初整個app的構想是輸入一張圖片，同學負責寫人臉辨識找出眼睛鼻子嘴巴的範圍，然後我負責在選取的範圍內random撒下control point，套用voronoi演算法，讓使用者可以很快調整臉部表情，不過因為有課程時間壓力，我的部分沒做得很完整，我們也沒整合在一起。",
+      coverImg: "",
+      youtubeLink: "https://www.youtube.com/embed/NUYvIrhZ9qY",
+      projectLink: ""
+    },
+    {
+      title: "ExponentialMap貼圖",
+      description: "研究所課程Project，使用ExponentialMap方法把2D貼圖貼到3D-Model上，影片中實際操作如何把一隻狗身馬頭的3D-Model貼上貼圖。",
+      coverImg: "https://c1.staticflickr.com/5/4260/34268020544_29dacc4714_o.png",
+      youtubeLink: "https://www.youtube.com/embed/cTgQ2-c3ozQ",
+      projectLink: ""
+    },
+    {
+      title: "3D-Model Skeleton Extraction",
+      description: "研究所課程Project，輸入一個3D-Model使用特定演算法一直縮減它的Mesh，直到萃取出3D-Model的骨架。",
+      coverImg: "",
+      youtubeLink: "https://www.youtube.com/embed/wQeneaS3w1Q",
+      projectLink: ""
+    },
+    {
+      title: "3D機器人",
+      description: "大學課程Project，使用openGL 內建Function畫多邊形 貼上貼圖 組合成一隻機器人，再建造Hierarchy讓機器人做一些動作。",
+      coverImg: "",
+      youtubeLink: "https://www.youtube.com/embed/JqaDRv9hxBo",
+      projectLink: ""
+    },
+    {
+      title: "3D迷宮",
+      description: "大學課程Project，把2D迷宮用OpenGL畫成3D",
+      coverImg: "",
+      youtubeLink: "https://www.youtube.com/embed/_FCwlXfGun4",
+      projectLink: ""
+    },
+    {
+      title: "3D雲霄飛車",
+      description: "大學課程Project，使用ogre做出3D雲霄飛車、加上軌道、skybox、有動畫的model、水的特效。",
+      coverImg: "",
+      youtubeLink: "https://www.youtube.com/embed/QW6OWeq5qWU",
+      projectLink: ""
+    },
+    {
+      title: "簡易PhotoShop",
+      description: "大學課程Project，使用OpenGL實作一些類似PhotoShop簡易的影像處理功能。",
+      coverImg: "",
+      youtubeLink: "https://www.youtube.com/embed/BIcD-3hFI1k",
+      projectLink: ""
+    }],
+    android: [{
+      title: "好事地圖",
+      description: "好事地圖是一個把自力更生，但卻相對弱勢的族群資料收集在一起，有愛心的使用者可以使用定位功能，幫助附近需要幫助的人。 等兵單的時候看到布丁三姊弟的新聞，突然靈光一閃想到這個主意，上網搜尋一下發現已經有人做了，而且做的還不錯，於是我主動跟對方說要幫忙，但沒做到太多的事情，只負責幫忙更新FacebookAPI和修正Google定位bug。",
+      coverImg: "https://lh3.ggpht.com/8uPpnikxioBhQRZHI-h1RFH-iwg_5jN6KK8LqVaDCL8fnAfEoYqiVWR4SfsF0NnI-6k=h900-rw",
+      projectLink: "https://play.google.com/store/apps/details?id=lab.prada.android.goodthingsmap&hl=zh_TW",
+      youtubeLink: ""
+    },
+    {
+      title: "Airsig使用者數據收集",
+      description: "等兵單時接的case，幫Airsig公司做使用者數據收集的App (Airsig公司是做手機空中簽名 解鎖螢幕的新創公司)，因為這個APP是做數據收集而已，並沒有上架或公開。",
+      coverImg: "https://c1.staticflickr.com/5/4286/34947329552_7351346610_o.png",
+      youtubeLink: "",
+      projectLink: "https://c1.staticflickr.com/5/4286/34947329552_7351346610_o.png"
+    }],
+    studyproject: [{
+      title: "大學專題-校務會議系統",
+      description: "使用Unity + Android/iOS 原生開源App Mupdf製作，跨平台(Windows、Android、iOS)運作，學校希望透過會議的數位化，減少會議中龐大的紙張使用量，且防止重要的會議文件流出，也能讓使用者能在會議開始前，存取需要會議前先閱讀的文件，並在會議結束自動刪除重要會議文件。",
+      coverImg: "",
+      projectLink: "",
+      youtubeLink: "https://www.youtube.com/embed/iPoDKToUTxY"
+    }],
+  };
+
+
+var vm = new Vue({
+  el:"#portfolio",
+  data:{
+    nowtab: "web",
+    nowindex: 0,
+    portfolioData: portfolioData,
+    span: 930
+  },
+  computed:{
+    computed_left(){
+      var result={"left": (-this.nowindex * this.span) + "px"};
+      console.log(result);
+      return result;
+    }
+  },
+  updated: function () {
+    //reset portfolio.yt-with-Cover
+    this.hideYTplayer();
+  },
+  methods: {
+    // 切換tab初始化 yt css
+    setnowtab(t){
+      //set tab
+      this.nowtab = t;
+      //reset index
+      this.nowindex = 0;
+    },
+    delta(d){
+      this.nowindex = (this.nowindex + d + this.portfolioData[this.nowtab].length) % this.portfolioData[this.nowtab].length;
+    },
+    bg_css(url){
+      return {
+        "background-image": "url(" + url + ")"
+      };
+    },
+    show(){
+      var ytCover = document.getElementById("youtubeCover");
+      var ytPlayer = document.getElementById("yt-with-Cover");
+      
+      //hide cover photo
+      if(ytCover)
+      {
+        ytCover.style.opacity = 0;
+        ytCover.style.zIndex = -1;
+      }
+      //show youtube player
+      if(ytPlayer)
+      {
+        ytPlayer.style.opacity= 1;
+        ytPlayer.style.zIndex = 1;
+        //auto play youtube
+        ytPlayer.src = ytPlayer.src+"&autoplay=1";
+      }
+    },
+    hideYTplayer(){
+      var ytCover = document.getElementById("youtubeCover");
+      var ytPlayer = document.getElementById("yt-with-Cover");
+      //show cover photo
+      if(ytCover)
+      {
+        ytCover.style.opacity = 1;
+        ytCover.style.zIndex = 1;
+      }
+      //show youtube player (no cover photo means only youtube link)
+      else if (this.portfolioData[this.nowtab][this.nowindex].coverImg === ""){
+        var player = document.getElementById("yt-without-Cover");
+        player.style.opacity = 1;
+        player.style.zIndex = 1;
+      }
+      //hide youtube player
+      if(ytPlayer)
+      {
+        ytPlayer.style.opacity = 0;
+        ytPlayer.style.zIndex = -1;
+      }
+    },
+  }
+});
+// console.log(ytCover);
+
+// start animation when page bottom reach
+$(window).scroll(function(evt){
+  // console.log("ScrollBottom:"+$(window).scrollTop()+$(window).height() + " profile:"+$("#profile").outerHeight() +" portfolio:"+ $("#portfolio").outerHeight());
+  if ( $(window).scrollTop()+$(window).height() < ($("#profile").outerHeight()+$("#portfolio").outerHeight()+($("#exp").outerHeight() - $("#exp").height())/2) ){
+    // console.log(111);
+      $("#exp").children('.container').addClass("hidden");
+      $(".line_vertical").removeClass("anismokefly");
+      $(".line_horizontal").removeClass("aniboxline");
+      $(".box").removeClass("aniboxline");
+      $(".rocket").removeClass("anirocketfly");
+      $(".somke1").removeClass("anismoke1fly");
+      $(".somke2").removeClass("anismoke2fly");
+      $(".somke3").removeClass("anismoke3fly");
+  }
+  else
+    {
+      // console.log(222);
+      $("#exp").children('.container').removeClass("hidden");
+      $(".line_vertical").addClass("anismokefly");
+      $(".line_horizontal").addClass("aniboxline");
+      $(".box").addClass("aniboxline");
+      $(".rocket").addClass("anirocketfly");
+      $(".somke1").addClass("anismoke1fly");
+      $(".somke2").addClass("anismoke2fly");
+      $(".somke3").addClass("anismoke3fly");
+    }
+});
+
+if ($(window).width() > 1000) {
+    var s = skrollr.init()
+}
+//var s = skrollr.init();
